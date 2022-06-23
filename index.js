34fb95a995a1e32e996511f46e97e87cca2ff8dd
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-06-22 22:25:36 -0700

    added init function for prompting user for manager info first, then what employee that they would like to add next

--- index.js
+++ index.js
@@ -2,4 +2,80 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Manager = require("./lib/manager");
-const renderTeam = require('./src/html-template')
+const renderTeam = require('./src/html-template');
+
+const teamMemberArr = [];
+
+const init = () => {
+    ///calls create Manager first to start team construction
+    const createManager = () => {
+        inquirer
+        .prompt (
+            [{
+                type: 'input',
+                name: 'id',
+                message: "What is the manager's id?",
+
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the manager's name?",
+
+            },
+
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "What is the manager's Office Number?",
+
+            },
+            ]
+        )
+        .then(answers => {
+            const manager = new Manager(
+                answers.id,
+                answers.name,
+                answers.email,
+                answers.officeNumber,
+            );
+            teamMemberObjArr.push(manager)
+            addEmployees()
+        })
+    };
+
+
+
+   /// prompts user what they want to add to directory next  
+    function addEmployees (){
+        inquirer
+        .prompt(
+            [
+                {
+                    type: 'list',
+                    name: 'choice',
+                    message: "What employees would you like to add?",
+                    choices: ["Engineer", "Intern", "I'm Done"],
+    
+                },
+            ]
+            )
+
+        .then(answer => {
+            switch(answer.choice){
+                case 'Engineer':
+                    createEngineer();
+                    break;
+                case 'Intern':
+                    creatIntern();
+                    break;
+                default:
+                    buildTeam();
+                    break;
+            }
+        })
+    }
+
+
+
+};
